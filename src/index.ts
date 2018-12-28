bdbe148f48799870fa12b63fc860a1aa2d2e0ba5
chengaoyuan <chen.gaoyuan@qq.com>
2018-12-29 01:28:04 +0800

    no message

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
 import argnames from "@chengaoyuan/argnames";
+import prepare from "@chengaoyuan/prepare";
+
+export function Cacheable({
+    name = "", // 如果不传
+    key = "",
+    expire = 1,
+    condition = ""
+}) {
+    type FuncType = (...args: any[]) => Promise<any>;
+    type DesType = TypedPropertyDescriptor<FuncType>;
+    return (target: any, methodName: string, des: DesType) => {
+        //
+        console.log(target.constructor.name);
+    };
+}
+
+class A {
+    @Cacheable({})
+    async b() {
+        return null;
+    }
+}
