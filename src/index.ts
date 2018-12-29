8706cc18d52db6293b6e7c0c170469fd61f02cb0
chengaoyuan <chen.gaoyuan@qq.com>
2018-12-30 03:18:41 +0800

    实现Cacheable

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,45 @@
 import argnames from "@chengaoyuan/argnames";
-import prepare from "@chengaoyuan/prepare";
+
+export interface CacheProtocol {
+    get(cache: string, key: string, cb: (value: any | undefined) => void): void;
+    set(cache: string, key: string, expire: undefined | number, value: any): void;
+    del(cache: string, key: string): void;
+    clear(cache: string): void;
+}
+const $info$: { protocol?: CacheProtocol } = {};
 
 type DescriptorType = TypedPropertyDescriptor<(...args: any[]) => Promise<any>>;
-type DecoratorType = (target: any, methodName: string, des: DescriptorType) => void;
-export function Cacheable(cache: string): DecoratorType;
-export function Cacheable({  }: {}): DecoratorType;
-
-export function Cacheable(...args: any[]) {
+export function Cacheable(config: { cache: string; key: string; expire: undefined | number; condition?: string }) {
     return (target: any, methodName: string, des: DescriptorType) => {
-        //
-        console.log(target.constructor.name);
+        const args = argnames(des.value as Function).join(",");
+        des.value = new Function(
+            "$function$",
+            "$info$",
+            "$cache$",
+            `
+            return function(${args}) {
+                var $self$ = this;
+                var $key$ = ${config.key};
+                return new Promise(function(resolve, reject){
+                    var $protocol$ = $info$.protocol;
+                    $protocol$.get($cache$, $key$, function($cacheValue$) {
+                        if ($cacheValue$ !== undefined) {
+                            resolve($cacheValue$);
+                        } else {
+                            $function$.call($self$${args ? ", " + args : ""})
+                                    .then(function($retValue$){
+                                        if ($retValue$ !== undefined) {
+                                            if(${config.condition}) {
+                                                $protocol$.set($cache$, $key$, ${config.expire}, $retValue$);
+                                            }
+                                        }
+                                        resolve($retValue$);
+                                    }, reject);
+                        }
+                    });
+                })
+            }
+        `
+        )(des.value, $info$, config.cache);
     };
-}
-
-class A {
-    @Cacheable({})
-    async b() {
-        return null;
-    }
 }
