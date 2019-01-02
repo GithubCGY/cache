2346d711b109735ebb52381aa406987f80c4eeb2
chengaoyuan <chen.gaoyuan@qq.com>
2019-01-02 11:14:47 +0800

    发布

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import argnames from "@chengaoyuan/argnames";
+import CacheProtocol from "./CacheProtocol";
 
-export interface CacheProtocol {
-    get(cache: string, key: string, cb: (value: any | undefined) => void): void;
-    set(cache: string, key: string, expire: undefined | number, value: any): void;
-    del(cache: string, key: string): void;
-    clear(cache: string): void;
-}
 const $info$: { protocol?: CacheProtocol } = {};
+
+export function CacheInit(protocol: CacheProtocol) {
+    $info$.protocol = protocol;
+}
 
 type DescriptorType = TypedPropertyDescriptor<(...args: any[]) => Promise<any>>;
 
@@ -126,69 +125,4 @@ export function CacheEvictAll(config: { cache: string; condition?: string }) {
         `
         )(des.value, $info$, config.cache);
     };
-}
-
-import { RedisClient } from "redis";
-function errorPrint(err: Error | null) {
-    if (err) {
-        console.error(err);
-    }
-}
-
-export default class RedisCache implements CacheProtocol {
-    redisClient: RedisClient;
-    constructor(redisClient: RedisClient) {
-        this.redisClient = redisClient;
-    }
-    get(cache: string, key: string, cb: (value: any | undefined) => void): void {
-        this.redisClient.get(`${cache}::${key}`, function(err, reply) {
-            if (err) {
-                errorPrint(err);
-                cb(undefined);
-            } else {
-                try {
-                    var value = JSON.parse(reply);
-                } catch (parseErr) {
-                    errorPrint(parseErr);
-                    cb(undefined);
-                    return;
-                }
-                cb(value);
-            }
-        });
-    }
-    set(cache: string, key: string, expire: undefined | number, value: any): void {
-        try {
-            var data = JSON.stringify(value);
-        } catch (err) {
-            errorPrint(err);
-            return;
-        }
-        if (expire === undefined) {
-            this.redisClient.set(`${cache}::${key}`, data, errorPrint);
-        } else {
-            this.redisClient.setex(`${cache}::${key}`, expire, data, errorPrint);
-        }
-    }
-    del(cache: string, key: string): void {
-        this.redisClient.del(`${cache}::${key}`, errorPrint);
-    }
-    clear(cache: string): void {
-        this.scanAndDel(`${cache}::*`, "0");
-    }
-    scanAndDel(match: string, cursor: string) {
-        this.redisClient.scan(cursor, "MATCH", match, "COUNT", "100", (err, reply) => {
-            if (err) {
-                errorPrint(err);
-            } else {
-                const [newCursor, keys] = reply;
-                if (keys.length) {
-                    this.redisClient.del(keys);
-                }
-                if (newCursor !== "0") {
-                    this.scanAndDel(match, newCursor);
-                }
-            }
-        });
-    }
 }
